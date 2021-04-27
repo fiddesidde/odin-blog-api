25ed05e778738f64027d079953c2099bf3074af5
fiddesidde <fredrik.mellberg@gmail.com>
2021-04-27 21:33:39 +0200

    Add post model

---
+++ models/post.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: String,
+  comments: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' },
+  postedAt: { type: Date, default: Date.now, required: true },
+  published: { type: Boolean, required: true },
+});
+
+module.exports = mongoose.model('Post', postSchema);
