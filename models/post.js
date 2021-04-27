8e3d62008a0bf6300efc9c23863923d045c03254
fiddesidde <fredrik.mellberg@gmail.com>
2021-04-27 21:36:22 +0200

    Add post model body field

--- models/post.js
+++ models/post.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  body: { type: String, required: true },
   author: String,
   comments: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' },
   postedAt: { type: Date, default: Date.now, required: true },
