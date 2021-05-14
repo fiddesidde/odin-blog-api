692a6eefbd122711146b7914066b8209d3bb5fb5
fiddesidde <fredrik.mellberg@gmail.com>
2021-05-14 22:24:18 +0200

    Add more routes

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
 const Post = require('./models/post');
+const Comment = require('./models/comment');
 
 const { PORT, DB_URL } = require('./utils/config');
+const comment = require('./models/comment');
 
 const app = express();
 
@@ -30,7 +32,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/v1/posts', async (req, res) => {
-  const posts = await Post.find({}).populate('comment');
+  const posts = await Post.find({});
 
   res.json(posts);
 });
@@ -47,6 +49,62 @@ app.post('/api/v1/posts', async (req, res) => {
   const savedPost = await post.save();
 
   res.status(201).json(savedPost);
+});
+
+app.get('/api/v1/posts/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const post = await Post.findById(id).populate('comments');
+
+  res.json(post);
+});
+
+// get all comments on a post
+app.get('/api/v1/posts/:id/comments', async (req, res) => {
+  const { id } = req.params;
+
+  const comments = await Comment.find({ post: id });
+
+  res.json(comments);
+});
+
+// post a comment on a post
+app.post('/api/v1/posts/:id/comments', async (req, res) => {
+  const { body, author } = req.body;
+  const post = req.params.id;
+
+  const comment = new Comment({
+    body,
+    author,
+    post,
+  });
+
+  const updatedPost = await Post.findByIdAndUpdate(post, {
+    $push: { comments: comment },
+  });
+
+  const savedComment = await comment.save();
+
+  res.status(201).json(savedComment);
+});
+
+app.delete('/api/v1/posts/:postId/comments/:commentId', async (req, res) => {
+  const { postId, commentId } = req.params;
+
+  const deletedComment = await Comment.findOneAndDelete({
+    _id: commentId,
+    post: postId,
+  });
+
+  console.log(deletedComment);
+  if (deletedComment) {
+    res.json({
+      status: 'success',
+      action: `deleted comment ${commentId}`,
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
